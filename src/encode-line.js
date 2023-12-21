63cd6191dd3344c30203c38768a228baec264da9
AlekPet <alexepetrof@gmail.com>
2023-12-21 21:25:16 +0300

    feat: implemented tasks get-email-domain mac-address matrix-elements-sum

--- src/encode-line.js
+++ src/encode-line.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given a string, return its encoding version.
@@ -11,10 +11,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
+  throw new NotImplementedError("Not implemented");
   // remove line with error and write your code here
+  // const objects_char = {};
+  // const chars = new Set(str.split("")).forEach(
+  //   (char) => (objects_char[char] = str.split(char).length - 1)
+  // );
+
+  // let result = "";
+  // for (let x in objects_char) {
+  //   const value = objects_char[x];
+  //   result += value === 1 ? "" + x : value + x;
+  // }
+  // return result;
 }
 
 module.exports = {
-  encodeLine
+  encodeLine,
 };
